51523c5a85af21e30cd2bcb1eea82693d07886cd
Austin Park <iam.austin@ymail.com>
2012-12-13 18:17:46 -0500

    initial revision

---
+++ Calculator/GraphingViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphingViewController.h
+//  Calculator
+//
+//  Created by Austin on 12/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphingViewController : UIViewController
+
+@end
---
+++ Calculator/GraphingViewController.m
@@ -0,0 +1,26 @@
+//
+//  GraphingViewController.m
+//  Calculator
+//
+//  Created by Austin on 12/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GraphingViewController.h"
+
+@interface GraphingViewController()
+
+@end
+
+@implementation GraphingViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
